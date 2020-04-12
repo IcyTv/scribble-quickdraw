ca4002527b0d1ff74be40f14b47d527bca45ff9b
Michael Finger <michael.finger@icytv.de>
2020-04-13 00:19:13 +0200

    CI Change

--- src/test/java/de/icytv/scribble/SQLTest.java
+++ src/test/java/de/icytv/scribble/SQLTest.java
@@ -1,29 +1,22 @@
 package de.icytv.scribble;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import de.icytv.scribble.sql.SQLQuery;
 
 @DisplayName("Sql tests")
 public class SQLTest {
 
-	@Test
-	@DisplayName("Query")
-	public void testQuery() throws SQLException {
-		ResultSet res = SQLQuery.query("users", "users");
-		assertThat(res.next()).isTrue();
-	}
+	// TODO removed for CI integration, adding once postgres in CI
+	// @Test
+	// @DisplayName("Query")
+	// public void testQuery() throws SQLException {
+	// 	ResultSet res = SQLQuery.query("users", "users");
+	// 	assertThat(res.next()).isTrue();
+	// }
 
-	@Test
-	@DisplayName("Query All")
-	public void testQueryAll() throws SQLException {
-		assertThat(SQLQuery.queryAll("users").next()).isTrue();
-	}
+	// @Test
+	// @DisplayName("Query All")
+	// public void testQueryAll() throws SQLException {
+	// 	assertThat(SQLQuery.queryAll("users").next()).isTrue();
+	// }
 
 }
