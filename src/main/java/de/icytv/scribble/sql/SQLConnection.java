f1e53fe8e506d305b4a8692b1e36ef1ef7a0f681
Michael Finger <michael.finger@icytv.de>
2020-04-13 02:04:53 +0200

    fixed port and postgres_db args

--- src/main/java/de/icytv/scribble/sql/SQLConnection.java
+++ src/main/java/de/icytv/scribble/sql/SQLConnection.java
@@ -20,13 +20,15 @@ public class SQLConnection {
 
 	private SQLConnection() {
 		try {
-			// conn = DriverManager.getConnection(HTTPServer.POSTGRES_URL, HTTPServer.POSTGRES_USER,
-			// 		HTTPServer.POSTGRES_PW);
-			String port = (System.getenv("POSTGRES_PORT") == null) ? "5432": System.getenv("POSTGRES_PORT");
-			String db = (System.getenv("PSOTGRES_DB") == null) ? "scribble": System.getenv("POSTGRES_DB");
-			String url = (System.getenv("POSTGRES_URL") == null) ? "jdbc:postgresql://localhost:" + port + "/" + db:  "jdbc:postgresql://" + System.getenv("POSTGRES_URL") + ":" + port + "/" + db;
-			String user = (System.getenv("POSTGRES_USER") == null) ? "postgres": System.getenv("POSTGRES_USER");
-			String pw = (System.getenv("POSTGRES_PW") == null) ? "postgres": System.getenv("POSTGRES_PW");
+			// conn = DriverManager.getConnection(HTTPServer.POSTGRES_URL,
+			// HTTPServer.POSTGRES_USER,
+			// HTTPServer.POSTGRES_PW);
+			String port = (System.getenv("POSTGRES_PORT") == null) ? "5432" : System.getenv("POSTGRES_PORT");
+			String db = (System.getenv("POSTGRES_DB") == null) ? "scribble" : System.getenv("POSTGRES_DB");
+			String url = (System.getenv("POSTGRES_URL") == null) ? "jdbc:postgresql://localhost:" + port + "/" + db
+					: "jdbc:postgresql://" + System.getenv("POSTGRES_URL") + ":" + port + "/" + db;
+			String user = (System.getenv("POSTGRES_USER") == null) ? "postgres" : System.getenv("POSTGRES_USER");
+			String pw = (System.getenv("POSTGRES_PW") == null) ? "postgres" : System.getenv("POSTGRES_PW");
 			System.out.println(url + "   " + user + " " + pw);
 			conn = DriverManager.getConnection(url, user, pw);
 			conn.setAutoCommit(true);
@@ -35,7 +37,7 @@ public class SQLConnection {
 		} catch (SQLException e) {
 			// Catch statement because of use in static final context
 			e.printStackTrace();
-			//throw new IllegalStateException(e.getMessage());
+			// throw new IllegalStateException(e.getMessage());
 		}
 	}
 
